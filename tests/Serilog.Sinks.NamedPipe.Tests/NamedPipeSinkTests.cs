638800e2b0b733596109bd82b3ca42e48c72d303
Michael Monsour <michael@lethek.net>
2023-07-21 13:39:42 +1000

    Added and updated unit tests

--- tests/Serilog.Sinks.NamedPipe.Tests/NamedPipeSinkTests.cs
+++ tests/Serilog.Sinks.NamedPipe.Tests/NamedPipeSinkTests.cs
@@ -16,11 +16,11 @@ public class NamedPipeSinkTests
     {
         string pipeName = GeneratePipeName();
         var pipeFactory = NamedPipeSink.CreateNamedPipeServerFactory(pipeName);
-        using var sink = new NamedPipeSink(pipeFactory, Encoding.UTF8, null, 100);
+        using var sink = new NamedPipeSink(pipeFactory, null, null, 100);
 
-        await using var client = new NamedPipeClientStream(".", pipeName);
+        await using var client = new NamedPipeClientStream(pipeName);
         await client.ConnectAsync();
-        using var reader = new StreamReader(client, Encoding.UTF8);
+        using var reader = new StreamReader(client);
         Assert.True(client.IsConnected);
 
         sink.Emit(CreateEvent("Hello unit test"));
@@ -34,11 +34,11 @@ public class NamedPipeSinkTests
     {
         string pipeName = GeneratePipeName();
         var pipeFactory = NamedPipeSink.CreateNamedPipeClientFactory(pipeName);
-        using var sink = new NamedPipeSink(pipeFactory, Encoding.UTF8, null, 100);
+        using var sink = new NamedPipeSink(pipeFactory, null, null, 100);
 
         await using var server = new NamedPipeServerStream(pipeName);
         await server.WaitForConnectionAsync();
-        using var reader = new StreamReader(server, Encoding.UTF8);
+        using var reader = new StreamReader(server);
         Assert.True(server.IsConnected);
 
         sink.Emit(CreateEvent("Hello unit test"));
@@ -48,20 +48,20 @@ public class NamedPipeSinkTests
 
 
     [Fact]
-    public async Task NamedPipeServer_WhenNamedPipeIsBroken_ShouldReconnect()
+    public async Task NamedPipeClient_WhenNamedPipeIsBroken_ShouldReconnect()
     {
         string pipeName = GeneratePipeName();
-        var pipeFactory = NamedPipeSink.CreateNamedPipeServerFactory(pipeName);
-        using var sink = new NamedPipeSink(pipeFactory, Encoding.UTF8, null, 100);
+        var pipeFactory = NamedPipeSink.CreateNamedPipeClientFactory(pipeName);
+        using var sink = new NamedPipeSink(pipeFactory, null, null, 100);
 
-        //Create our first client connection to the pipe, then break the pipe by allowing the connection to be disposed
-        await using (var client = new NamedPipeClientStream(pipeName)) {
-            await client.ConnectAsync();
-            using var reader = new StreamReader(client, Encoding.UTF8);
+        //Create an initial connection to read from the pipe, then break the pipe by allowing the connection to be disposed
+        await using (var client = new NamedPipeServerStream(pipeName)) {
+            await client.WaitForConnectionAsync();
+            using var reader = new StreamReader(client);
 
-            sink.Emit(CreateEvent("Message while connected 1"));
+            sink.Emit(CreateEvent("Message while connected"));
 
-            Assert.Equal("Message while connected 1", await RenderNextLogEventAsync(reader));
+            Assert.Equal("Message while connected", await RenderNextLogEventAsync(reader));
         }
 
 
@@ -69,18 +69,113 @@ public class NamedPipeSinkTests
         sink.Emit(CreateEvent("Detect broken pipe"));
 
 
-        //Create our second client connection to the pipe
-        await using (var client = new NamedPipeClientStream(".", pipeName)) {
-            await client.ConnectAsync();
-            using var reader = new StreamReader(client, Encoding.UTF8);
+        //Create a second connection to read from the pipe
+        await using (var client = new NamedPipeServerStream(pipeName)) {
+            await client.WaitForConnectionAsync();
+            using var reader = new StreamReader(client);
 
-            //Logs such as the following which were queued while the pipe was not connected, will be delivered here upon reconnection
+            //Logs (such as the following) which were queued while the pipe was not connected, are finally delivered here upon reconnection
             Assert.Equal("Detect broken pipe", await RenderNextLogEventAsync(reader));
 
-            sink.Emit(CreateEvent("Message while connected 2"));
+            sink.Emit(CreateEvent("Another message while connected"));
 
-            Assert.Equal("Message while connected 2", await RenderNextLogEventAsync(reader));
+            Assert.Equal("Another message while connected", await RenderNextLogEventAsync(reader));
         }
+    }
+
+
+    [Fact]
+    public async Task NamedPipeServer_WhenNamedPipeIsBroken_ShouldReconnect()
+    {
+        string pipeName = GeneratePipeName();
+        var pipeFactory = NamedPipeSink.CreateNamedPipeServerFactory(pipeName);
+        using var sink = new NamedPipeSink(pipeFactory, null, null, 100);
+
+        //Create an initial connection to read from the pipe, then break the pipe by allowing the connection to be disposed
+        await using (var client = new NamedPipeClientStream(pipeName)) {
+            await client.ConnectAsync();
+            using var reader = new StreamReader(client);
+
+            sink.Emit(CreateEvent("Message while connected"));
+
+            Assert.Equal("Message while connected", await RenderNextLogEventAsync(reader));
+        }
+
+
+        //Logging while the pipe is broken allows the sink to detect and start reconnecting
+        sink.Emit(CreateEvent("Detect broken pipe"));
+
+
+        //Create a second connection to read from the pipe
+        await using (var client = new NamedPipeClientStream(pipeName)) {
+            await client.ConnectAsync();
+            using var reader = new StreamReader(client);
+
+            //Logs (such as the following) which were queued while the pipe was not connected, are finally delivered here upon reconnection
+            Assert.Equal("Detect broken pipe", await RenderNextLogEventAsync(reader));
+
+            sink.Emit(CreateEvent("Another message while connected"));
+
+            Assert.Equal("Another message while connected", await RenderNextLogEventAsync(reader));
+        }
+    }
+
+
+    [Fact]
+    public async Task NamedPipeServer_WhenEmittingWhileDisconnected_ShouldQueueLogEventsUpToCapacity()
+    {
+        string pipeName = GeneratePipeName();
+        var pipeFactory = NamedPipeSink.CreateNamedPipeServerFactory(pipeName);
+        using var sink = new NamedPipeSink(pipeFactory, null, null, 10);
+
+        //Emit 20 log events while the pipe is disconnected
+        for (int i = 0; i < 20; i++) {
+            sink.Emit(CreateEvent(i.ToString()));
+        }
+
+        await using var client = new NamedPipeClientStream(pipeName);
+        await client.ConnectAsync();
+        using var reader = new StreamReader(client);
+
+        //We set capacity to 10, so we should have only the first 10 log events queued up; the rest should have been dropped
+        Assert.Equal(10, sink.Channel.Reader.Count);
+
+        //Allow those 10 queued log events to be delivered
+        for (int i = 0; i < 10; i++) {
+            Assert.Equal(i.ToString(), await RenderNextLogEventAsync(reader));
+        }
+
+        //There should not be any more log events queued up
+        Assert.Equal(0, sink.Channel.Reader.Count);
+    }
+
+
+    [Fact]
+    public async Task NamedPipeClient_WhenEmittingWhileDisconnected_ShouldQueueLogEventsUpToCapacity()
+    {
+        string pipeName = GeneratePipeName();
+        var pipeFactory = NamedPipeSink.CreateNamedPipeClientFactory(pipeName);
+        using var sink = new NamedPipeSink(pipeFactory, null, null, 10);
+
+        //Emit 20 log events while the pipe is disconnected
+        for (int i = 0; i < 20; i++) {
+            sink.Emit(CreateEvent(i.ToString()));
+        }
+
+        await using var client = new NamedPipeServerStream(pipeName);
+        await client.WaitForConnectionAsync();
+        using var reader = new StreamReader(client);
+
+        //We set capacity to 10, so we should have only the first 10 log events queued up; the rest should have been dropped
+        Assert.Equal(10, sink.Channel.Reader.Count);
+
+        //Allow those 10 queued log events to be delivered
+        for (int i = 0; i < 10; i++) {
+            Assert.Equal(i.ToString(), await RenderNextLogEventAsync(reader));
+        }
+
+        //There should not be any more log events queued up
+        Assert.Equal(0, sink.Channel.Reader.Count);
     }
 
 
